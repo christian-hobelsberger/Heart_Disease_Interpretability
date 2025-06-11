509daa6af1e7621ce8fe2931c54e55ec7dfb6ced
Christian Hobelsberger <chobelsberger@gmail.com>
2025-06-11 13:29:17 +0200

    finished data loading

--- courselib/utils/loaders.py
+++ courselib/utils/loaders.py
@@ -5,9 +5,9 @@ def load_heart_data():
     Load the Heart Disease dataset from the UCI Machine Learning Repository.
 
     Returns:
-        Tuple[pd.DataFrame, pd.Series]: Features and target.
+        Tuple[pd.DataFrame, pd.DataFrame]: Features and target.
     """
     heart = fetch_ucirepo(id=45)
     X = heart.data.features
-    y = heart.data.targets.iloc  # Select 'target' column
+    y = heart.data.targets
     return X, y
